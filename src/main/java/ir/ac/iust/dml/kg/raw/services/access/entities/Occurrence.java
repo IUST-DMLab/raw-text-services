5d03b3155cef62b3ba973094804875ded2121752
Majid Asgari <majid.asgari@gmail.com>
2017-05-27 10:25:40 +0430

    index on assignee

--- src/main/java/ir/ac/iust/dml/kg/raw/services/access/entities/Occurrence.java
+++ src/main/java/ir/ac/iust/dml/kg/raw/services/access/entities/Occurrence.java
@@ -3,10 +3,10 @@ package ir.ac.iust.dml.kg.raw.services.access.entities;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
-import org.springframework.data.mongodb.core.index.*;
 
 import java.util.List;
 
@@ -35,6 +35,7 @@ public class Occurrence {
   @Indexed
   private Boolean approved;
   @DBRef
+  @Indexed
   private User assignee;
 
   public ObjectId getUid() {
